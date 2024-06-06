769bb0cc10ea09c179e9ff7a13f785d38c7896d3
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-06-06 14:20:40 +0530

    Updated tshirts page

--- pages/tshirts.js
+++ pages/tshirts.js
@@ -1,9 +1,115 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Tshirts = () => {
   return (
     <div>
-      Tshirts
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            <Link href={'/product/wear-from-haven'}><div className="p-4 xl:w-1/10 cursor-pointer shadow-lg m-10 block relative rounded overflow-hidden">
+                <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block " src="https://m.media-amazon.com/images/I/81wFLUDzAbL._SY741_.jpg" />
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            </Link>
+            <Link href={'/product/wear-from-haven'}><div className="p-4 xl:w-1/10 cursor-pointer shadow-lg m-10 block relative rounded overflow-hidden">
+                <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/81wFLUDzAbL._SY741_.jpg" />
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            </Link>
+            <Link href={'/product/wear-from-haven'}><div className="p-4 xl:w-1/10 cursor-pointer shadow-lg m-10 block relative rounded overflow-hidden">
+                <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/81wFLUDzAbL._SY741_.jpg" />
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            </Link>
+            <Link href={'/product/wear-from-haven'}><div className="p-4 xl:w-1/10 cursor-pointer shadow-lg m-10 block relative rounded overflow-hidden">
+                <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/81wFLUDzAbL._SY741_.jpg" />
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            </Link>
+            <Link href={'/product/wear-from-haven'}><div className="p-4 xl:w-1/10 cursor-pointer shadow-lg m-10 block relative rounded overflow-hidden">
+                <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/81wFLUDzAbL._SY741_.jpg" />
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            </Link>
+            <Link href={'/product/wear-from-haven'}><div className="p-4 xl:w-1/10 cursor-pointer shadow-lg m-10 block relative rounded overflow-hidden">
+                <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/81wFLUDzAbL._SY741_.jpg" />
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            </Link>
+            <Link href={'/product/wear-from-haven'}><div className="p-4 xl:w-1/10 cursor-pointer shadow-lg m-10 block relative rounded overflow-hidden">
+                <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/81wFLUDzAbL._SY741_.jpg" />
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            </Link>
+            <Link href={'/product/wear-from-haven'}><div className="p-4 xl:w-1/10 cursor-pointer shadow-lg m-10 block relative rounded overflow-hidden">
+                <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/81wFLUDzAbL._SY741_.jpg" />
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            </Link>
+            <Link href={'/product/wear-from-haven'}><div className="p-4 xl:w-1/10 cursor-pointer shadow-lg m-10 block relative rounded overflow-hidden">
+                <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/81wFLUDzAbL._SY741_.jpg" />
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            </Link>
+            <Link href={'/product/wear-from-haven'}><div className="p-4 xl:w-1/10 cursor-pointer shadow-lg m-10 block relative rounded overflow-hidden">
+                <img alt="ecommerce" className="m-auto md:mx-0 h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/81wFLUDzAbL._SY741_.jpg" />
+              <div className="mt-4 text-center md:text-left">
+                <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">T-shirts</h3>
+                <h2 className="text-gray-900 title-font text-lg font-medium">Wear the code</h2>
+                <p className="mt-1">₹499</p>
+                <p className="mt-1">S, M, L, XL, XXL</p>
+              </div>
+            </div>
+            </Link>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
