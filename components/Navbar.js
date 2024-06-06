868f449766850e44a87308b5162a01c1b650b6a4
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-06-06 20:47:48 +0530

    Added sidebar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineShoppingCart, AiOutlineCloseCircle } from "react-icons/ai";
+import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
+import { IoBagCheckOutline } from "react-icons/io5";
+import {useRef} from 'react'
 
 
 const Navbar = () => {
+  const toggleCart = () =>{
+    if (ref.current.classList.contains('translate-x-full')){
+      ref.current.classList.remove('translate-x-full');
+      ref.current.classList.add('translate-x-0')
+    }
+    else if (!ref.current.classList.contains('translate-x-full')){
+      ref.current.classList.remove('translate-x-0')
+      ref.current.classList.add('translate-x-full');
+    }
+  }
+  const ref = useRef()
   return (
     <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md'>
       <div className="logo mx-5">
@@ -18,10 +32,24 @@ const Navbar = () => {
             <Link href={'/mugs'}><li>Mugs</li></Link>
         </ul>
       </div>
-      <div className='cart absolute right-0 top-4 mx-5'>
+      <div onClick={toggleCart} className='cursor-pointer cart absolute right-0 top-4 mx-5'>
         <AiOutlineShoppingCart className='text-xl md:text-2xl'/>
       </div>
-    </div>
+
+<div ref={ref} className="w-72 h-full sidecart absolute top-0 right-0 bg-red-100 px-8 py-10 transform transition-transform translate-x-full z-10">
+  <h2 className='font-bold text-xl text-center'>Shopping Cart</h2>
+  <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl"><AiOutlineCloseCircle /></span>
+  <ol className='list-decimal font-semibold'>
+    <li>
+      <div className="item flex my-5">
+      <div className='w-2/3 font-semibold'>Tshirt - Wear from Haven</div>
+      <div className='flex items-center justify-center font-semibold w-1/3 text-lg'><CiCircleMinus className='cursor-pointer' /><span className='mx-2 text-sm'>1</span><CiCirclePlus className='cursor-pointer'/></div>
+      </div>
+    </li>
+  </ol>
+  <button className="flex mx-auto mt-16 text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-sm"><IoBagCheckOutline className='m-1' />Checkout</button>
+</div>
+</div>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "@/styles/globals.css";
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return <>
+  <Head>
+  <meta name="viewport" content="width=device-width , initial-scale=1.0 , minimum-scale=1.0"/>
+  </Head>
   <Navbar/>
   <Component {...pageProps} />
   <Footer/>
