3f59fec5973805bdf38cea87bb879393533d877c
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-06-08 16:13:34 +0530

    Added signup pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,76 +1,126 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 import { AiOutlineShoppingCart, AiOutlineCloseCircle } from "react-icons/ai";
 import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
 import { IoBagCheckOutline } from "react-icons/io5";
-import { MdOutlineRemoveShoppingCart } from "react-icons/md";
-import { useRef } from 'react';
+import { MdOutlineRemoveShoppingCart, MdAccountCircle } from "react-icons/md";
+import { useRef } from "react";
 
 const Navbar = ({ cart, addToCart, removeFromCart, clearCart, subTotal }) => {
   // console.log(cart, addToCart, removeFromCart, clearCart, subTotal)
 
   const toggleCart = () => {
-    if (ref.current.classList.contains('translate-x-full')) {
-      ref.current.classList.remove('translate-x-full');
-      ref.current.classList.add('translate-x-0');
+    if (ref.current.classList.contains("translate-x-full")) {
+      ref.current.classList.remove("translate-x-full");
+      ref.current.classList.add("translate-x-0");
     } else {
-      ref.current.classList.remove('translate-x-0');
-      ref.current.classList.add('translate-x-full');
+      ref.current.classList.remove("translate-x-0");
+      ref.current.classList.add("translate-x-full");
     }
-  }
+  };
 
   const ref = useRef();
 
   return (
-    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md sticky top-0 bg-white z-10'>
+    <div className="flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md sticky top-0 bg-white z-10">
       <div className="logo mx-5">
-        <Link href={"/"}><Image src="/logo.png" alt="Logo" height={40} width={180} /></Link>
+        <Link href={"/"}>
+          <Image src="/logo.png" alt="Logo" height={40} width={180} />
+        </Link>
       </div>
       <div className="nav">
-        <ul className='flex items-center space-x-6 font-bold md:text-md'>
-          <Link href={'/tshirts'}><li>Tshirts</li></Link>
-          <Link href={'/hoodies'}><li>Hoodies</li></Link>
-          <Link href={'/stickers'}><li>Stickers</li></Link>
-          <Link href={'/mugs'}><li>Mugs</li></Link>
+        <ul className="flex items-center space-x-6 font-bold md:text-md">
+          <Link href={"/tshirts"}>
+            <li>Tshirts</li>
+          </Link>
+          <Link href={"/hoodies"}>
+            <li>Hoodies</li>
+          </Link>
+          <Link href={"/stickers"}>
+            <li>Stickers</li>
+          </Link>
+          <Link href={"/mugs"}>
+            <li>Mugs</li>
+          </Link>
         </ul>
       </div>
-      <div onClick={toggleCart} className='cursor-pointer cart absolute right-0 top-4 mx-5'>
-        <AiOutlineShoppingCart className='text-xl md:text-2xl' />
+      <div className="cursor-pointer cart absolute right-0 top-4 mx-5 flex">
+        <Link href={'/login'}><MdAccountCircle className="text-xl md:text-2xl mx-2" /></Link>
+        <AiOutlineShoppingCart
+          onClick={toggleCart}
+          className="text-xl md:text-2xl"
+        />
       </div>
 
-      <div ref={ref} className={`w-72 h-[100vh] sidecart absolute top-0 right-0 bg-red-100 px-8 py-10 transform transition-transform ${Object.keys(cart).length !==0 ? 'translate-x-0' : 'translate-x-full'}`}>
-        <h2 className='font-bold text-xl text-center'>Shopping Cart</h2>
-        <span onClick={toggleCart} className="absolute top-5 right-2 cursor-pointer text-2xl"><AiOutlineCloseCircle /></span>
-        <ol className='list-decimal font-semibold'>
-          {Object.keys(cart).length == 0 &&
-            <div className='my-4 font-semibold'>Your cart is empty!</div>}
+      <div
+        ref={ref}
+        className={`w-72 h-[100vh] sidecart absolute top-0 right-0 bg-red-100 px-8 py-10 transform transition-transform ${
+          Object.keys(cart).length !== 0 ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <h2 className="font-bold text-xl text-center">Shopping Cart</h2>
+        <span
+          onClick={toggleCart}
+          className="absolute top-5 right-2 cursor-pointer text-2xl"
+        >
+          <AiOutlineCloseCircle />
+        </span>
+        <ol className="list-decimal font-semibold">
+          {Object.keys(cart).length == 0 && (
+            <div className="my-4 font-semibold">Your cart is empty!</div>
+          )}
           {Object.keys(cart).map((k) => {
-            return <li key={k}>
-              <div className="item flex my-5">
-                <div className='w-2/3 font-semibold'>{cart[k].name}</div>
-                <div className='flex items-center justify-center font-semibold w-1/3 text-lg'>
-                  <CiCircleMinus className='cursor-pointer' onClick={() => removeFromCart(k, 1)} />
-                  <span className='mx-2 text-sm'>{cart[k].qty}</span>
-                  <CiCirclePlus className='cursor-pointer' onClick={() => addToCart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].variant)} />
+            return (
+              <li key={k}>
+                <div className="item flex my-5">
+                  <div className="w-2/3 font-semibold">{cart[k].name}</div>
+                  <div className="flex items-center justify-center font-semibold w-1/3 text-lg">
+                    <CiCircleMinus
+                      className="cursor-pointer"
+                      onClick={() => removeFromCart(k, 1)}
+                    />
+                    <span className="mx-2 text-sm">{cart[k].qty}</span>
+                    <CiCirclePlus
+                      className="cursor-pointer"
+                      onClick={() =>
+                        addToCart(
+                          k,
+                          1,
+                          cart[k].price,
+                          cart[k].name,
+                          cart[k].size,
+                          cart[k].variant
+                        )
+                      }
+                    />
+                  </div>
                 </div>
-              </div>
-            </li>
+              </li>
+            );
           })}
         </ol>
-        <div className='font-bold my-2'>Subtotal: ₹{subTotal}</div>
+        <div className="font-bold my-2">Subtotal: ₹{subTotal}</div>
 
-        <div className='flex'>
-        <Link href={'/checkout'}> <button className="flex mr-2 text-white bg-red-500 border-0 py-2 px-2 focus:outline-none hover:bg-red-600 rounded text-sm">
-            <IoBagCheckOutline className='m-1' />Checkout</button>
+        <div className="flex">
+          <Link href={"/checkout"}>
+            {" "}
+            <button className="flex mr-2 text-white bg-red-500 border-0 py-2 px-2 focus:outline-none hover:bg-red-600 rounded text-sm">
+              <IoBagCheckOutline className="m-1" />
+              Checkout
+            </button>
           </Link>
-          <button onClick={clearCart} className="flex mr-2 text-white bg-red-500 border-0 py-2 px-2 focus:outline-none hover:bg-red-600 rounded text-sm">
-            <MdOutlineRemoveShoppingCart className='m-1' />Clear Cart
+          <button
+            onClick={clearCart}
+            className="flex mr-2 text-white bg-red-500 border-0 py-2 px-2 focus:outline-none hover:bg-red-600 rounded text-sm"
+          >
+            <MdOutlineRemoveShoppingCart className="m-1" />
+            Clear Cart
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
---
+++ pages/forgot.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Forgot = () => {
+  return (
+    <div>
+<div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <img className="mx-auto h-10 w-auto" src="/logo.png" alt="Your Company"/>
+    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Reset your Password</h2>
+  </div>
+
+  <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form className="space-y-6" action="#" method="POST">
+      <div>
+        <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+        <div className="mt-2">
+        <input id="email" name="email" type="email" autocomplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 placeholder:px-2 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6" placeholder="Enter your Email ID"/>
+        </div>
+      </div>
+      <div>
+        <button type="submit" className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Continue</button>
+      </div>
+    </form>
+
+    <p className="mt-10 text-center text-sm text-gray-500">
+      Remember your Password?
+      <Link href={"/login"}className="font-semibold leading-6 text-red-600 hover:text-red-500"> Log in</Link>
+    </p>
+  </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Forgot
