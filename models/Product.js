73db7832095afe224fd5344915f4c9c3e820dae0
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-06-10 10:02:04 +0530

    Added Sign up validation

--- models/Product.js
+++ models/Product.js
@@ -11,5 +11,6 @@ const ProductSchema = new mongoose.Schema({
     availableQty: {type: String, required: true},
 }, {timestamps:true});
 
-mongoose.models = {}
-export default mongoose.model("Product", ProductSchema);
+// mongoose.models = {}
+// export default mongoose.model("Product", ProductSchema);
+export default mongoose.models.Product || mongoose.model("Product", ProductSchema);
