9d04e17245b6de41eba56def937f1893e66fec35
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-06-11 14:56:12 +0530

    Added payment gateway

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,66 +1,171 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
 import { AiOutlineShoppingCart, AiOutlineCloseCircle } from "react-icons/ai";
 import { CiCirclePlus, CiCircleMinus } from "react-icons/ci";
 import { IoBagCheckOutline } from "react-icons/io5";
 import { MdOutlineRemoveShoppingCart } from "react-icons/md";
 
-const Checkout = ({cart, subTotal, addToCart, removeFromCart}) => {
+const Checkout = ({ cart, subTotal, addToCart, removeFromCart }) => {
+  const initiatePayment = async() => {
+    let oid = Math.floor(Math.random() * Date.now());
+    const data = {cart, subTotal, oid, email: "email"};
+    let a = await fetch(`/api/pretransaction`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+    let txnRes = await a.json()
+      // console.log(txnRes)
+      let txnToken = txnRes.txnToken
+
+    var config = {
+      root: "",
+      flow: "DEFAULT",
+      data: {
+        orderId: oid /* update order id */,
+        token: txnToken /* update token value */,
+        tokenType: "TXN_TOKEN",
+        amount: subTotal /* update amount */,
+      },
+      handler: {
+        notifyMerchant: function (eventName, data) {
+          console.log("notifyMerchant handler function called");
+          console.log("eventName => ", eventName);
+          console.log("data => ", data);
+        },
+      },
+    };
+
+        window.Paytm.CheckoutJS.init(config)
+          .then(function onSuccess() {
+            // after successfully updating configuration, invoke JS Checkout
+            window.Paytm.CheckoutJS.invoke();
+          })
+          .catch(function onError(error) {
+            console.log("error => ", error);
+          });
+  };
   return (
-    <div className='container px-2 m-auto'>
-      <h1 className='font-bold text-3xl my-8 text-center'>Checkout</h1>
-      <h2 className='font-semibold text-xl'>1. Delivery Details</h2>
+    <div className="container px-2 m-auto">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0"
+        />
+      </Head>
+      <Script
+        type="application/javascript"
+        crossOrigin="anonymous"
+        src={`${process.env.NEXT_PUBLIC_PAYTM_HOST}/merchantpgpui/checkoutjs/merchants/${process.env.NEXT_PUBLIC_PAYTM_MID}.js`}
+      />
+      <h1 className="font-bold text-3xl my-8 text-center">Checkout</h1>
+      <h2 className="font-semibold text-xl">1. Delivery Details</h2>
       <div className="mx-auto flex my-2">
         <div className="px-2 w-1/2">
-        <div className="mb-4">
-        <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-        <input type="name" id="name" name="name" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-      </div>
+          <div className="mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+              Name
+            </label>
+            <input
+              type="name"
+              id="name"
+              name="name"
+              className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
         </div>
         <div className="px-2 w-1/2">
-        <div className="mb-4">
-        <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-        <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          <div className="mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+        </div>
       </div>
-        </div>
-        </div>
-        <div className="px-2 w-full">
+      <div className="px-2 w-full">
         <div className="mb-4">
-        <label htmlFor="address" className="leading-7 text-sm text-gray-600">Address</label>
-        <textarea name="address" id="address" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></textarea>
-      </div>
+          <label htmlFor="address" className="leading-7 text-sm text-gray-600">
+            Address
+          </label>
+          <textarea
+            name="address"
+            id="address"
+            className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          ></textarea>
         </div>
-        <div className="mx-auto flex my-2">
+      </div>
+      <div className="mx-auto flex my-2">
         <div className="px-2 w-1/2">
-        <div className="mb-4">
-        <label htmlFor="phone" className="leading-7 text-sm text-gray-600">Phone</label>
-        <input type="phone" id="phone" name="phone" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-      </div>
+          <div className="mb-4">
+            <label htmlFor="phone" className="leading-7 text-sm text-gray-600">
+              Phone
+            </label>
+            <input
+              type="phone"
+              id="phone"
+              name="phone"
+              className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
         </div>
         <div className="px-2 w-1/2">
-        <div className="mb-4">
-        <label htmlFor="city" className="leading-7 text-sm text-gray-600">City</label>
-        <input type="city" id="city" name="city" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          <div className="mb-4">
+            <label htmlFor="city" className="leading-7 text-sm text-gray-600">
+              City
+            </label>
+            <input
+              type="city"
+              id="city"
+              name="city"
+              className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+        </div>
       </div>
-        </div>
-        </div>
-        <div className="mx-auto flex my-2">
+      <div className="mx-auto flex my-2">
         <div className="px-2 w-1/2">
-        <div className="mb-4">
-        <label htmlFor="state" className="leading-7 text-sm text-gray-600">State</label>
-        <input type="state" id="state" name="state" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-      </div>
+          <div className="mb-4">
+            <label htmlFor="state" className="leading-7 text-sm text-gray-600">
+              State
+            </label>
+            <input
+              type="state"
+              id="state"
+              name="state"
+              className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
         </div>
         <div className="px-2 w-1/2">
-        <div className="mb-4">
-        <label htmlFor="pincode" className="leading-7 text-sm text-gray-600">PinCode</label>
-        <input type="pincode" id="pincode" name="pincode" className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          <div className="mb-4">
+            <label
+              htmlFor="pincode"
+              className="leading-7 text-sm text-gray-600"
+            >
+              PinCode
+            </label>
+            <input
+              type="pincode"
+              id="pincode"
+              name="pincode"
+              className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+        </div>
       </div>
-        </div>
-        </div>
-        <h2 className='font-semibold text-xl'>2. Review Cart Items & Pay</h2>
-        <div className="p-6 m-2">
+      <h2 className="font-semibold text-xl">2. Review Cart Items & Pay</h2>
+      <div className="p-6 m-2">
         <ol className="list-decimal font-semibold">
           {Object.keys(cart).length == 0 && (
             <div className="my-4 font-semibold">Your cart is empty!</div>
@@ -69,7 +174,9 @@ const Checkout = ({cart, subTotal, addToCart, removeFromCart}) => {
             return (
               <li key={k}>
                 <div className="item flex my-5">
-                  <div className="font-semibold">{cart[k].name}({cart[k].size}/{cart[k].variant})</div>
+                  <div className="font-semibold">
+                    {cart[k].name}({cart[k].size}/{cart[k].variant})
+                  </div>
                   <div className="flex items-center justify-center font-semibold w-1/3 text-lg">
                     <CiCircleMinus
                       className="cursor-pointer"
@@ -93,17 +200,24 @@ const Checkout = ({cart, subTotal, addToCart, removeFromCart}) => {
                 </div>
               </li>
             );
-            })}
+          })}
         </ol>
-          <span className='font-bold'>Subtotal: ₹{subTotal}</span>
+        <span className="font-bold">Subtotal: ₹{subTotal}</span>
       </div>
       <div className="mx-4">
-      <Link href={'/checkout'}> <button className="flex mr-2 text-white bg-red-500 border-0 py-2 px-2 focus:outline-none hover:bg-red-600 rounded text-sm">
-            <IoBagCheckOutline className='m-1' />Pay ₹{subTotal}</button>
-          </Link>
+        <Link href={"/checkout"}>
+          {" "}
+          <button
+            onClick={initiatePayment}
+            className="flex mr-2 text-white bg-red-500 border-0 py-2 px-2 focus:outline-none hover:bg-red-600 rounded text-sm"
+          >
+            <IoBagCheckOutline className="m-1" />
+            Pay ₹{subTotal}
+          </button>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
