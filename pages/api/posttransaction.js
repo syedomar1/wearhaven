3b10f30498c58bbd559492594a977ffc9975a600
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-06-14 10:14:37 +0530

    Added Paytm checksum tamper check

--- pages/api/posttransaction.js
+++ pages/api/posttransaction.js
@@ -1,27 +1,56 @@
 import Order from "../../models/Order";
 import Product from "@/models/Product";
 import connectDb from "@/middleware/mongoose";
+import PaytmChecksum from "paytmchecksum";
 
 const handler = async (req, res) => {
   let order;
   // Validate paytm checksum
-  // Update the order status into order table after checking transaction status
-  if(req.body.STATUS == 'TXN_SUCCESS'){
-    order = await Order.findOneAndUpdate({orderId: req.body.ORDERID},{status: "Paid", paymentInfo: JSON.stringify(req.body)})
-    let products = order.products
-    for(let slug in products){
-      // console.log(products[slug].qty)
-      await Product.findOneAndUpdate({slug:slug}, {$inc: {"availableQty": - products[slug].qty}})
-      // await products.save()
+  var paytmChecksum = "";
+  var paytmParams = {};
+  const received_data = req.body;
+  for (var key in received_data) {
+    if (key == "CHECKSUMHASH") {
+      paytmChecksum = received_data[key];
+    } else {
+      paytmParams[key] = received_data[key];
     }
   }
-  else if(req.body.STATUS == 'PENDING'){
-    order = await Order.findOneAndUpdate({orderId: req.body.ORDERID},{status: "Pending", paymentInfo: JSON.stringify(req.body)})
+
+  var isValidChecksum = PaytmChecksum.verifySignature(
+    paytmParams,
+    process.env.PAYTM_MKEY,
+    paytmChecksum
+  );
+  if (!isValidChecksum) {
+    res.status(500).send("Some Error Occured");
+    return;
+  }
+  // Update the order status into order table after checking transaction status
+  if (req.body.STATUS == "TXN_SUCCESS") {
+    order = await Order.findOneAndUpdate(
+      { orderId: req.body.ORDERID },
+      { status: "Paid", paymentInfo: JSON.stringify(req.body) }
+    );
+    let products = order.products;
+    for (let slug in products) {
+      // console.log(products[slug].qty)
+      await Product.findOneAndUpdate(
+        { slug: slug },
+        { $inc: { availableQty: -products[slug].qty } }
+      );
+      // await products.save()
+    }
+  } else if (req.body.STATUS == "PENDING") {
+    order = await Order.findOneAndUpdate(
+      { orderId: req.body.ORDERID },
+      { status: "Pending", paymentInfo: JSON.stringify(req.body) }
+    );
   }
   // Initiate shipping
   // Redirect user to order confirmation page
-  res.redirect('/order?clearCart=1&id='+order._id, 200)
-    // res.status(200).json({ body: req.body });
-  }
-  
+  res.redirect("/order?clearCart=1&id=" + order._id, 200);
+  // res.status(200).json({ body: req.body });
+};
+
 export default connectDb(handler);
